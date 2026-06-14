e897cf45eac436d52005dc9458ea067777dbeaf3
twlite <46562212+twlite@users.noreply.github.com>
2026-06-14 15:56:46 +0545

    feat: implement Application component for routing in main application

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './globals.css';
-import App from './App';
+import Application from './application';
 import { TooltipProvider } from '@/components/ui/tooltip';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TooltipProvider>
-      <App />
+      <Application />
     </TooltipProvider>
   </StrictMode>,
 );
